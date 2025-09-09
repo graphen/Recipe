52678af053404d2d361ec4f40c7a86a74fb17b80
Przemek Szamraj <przemyslaw.szamraj@pl.ibm.com>
2025-09-09 03:45:30 +0200

    Add base application classes

---
+++ src/main/java/com/szamraj/spring_guru_9_spring_webapp3/SpringGuru9SpringWebapp3Application.java
@@ -0,0 +1,13 @@
+package com.szamraj.spring_guru_9_spring_webapp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuru9SpringWebapp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuru9SpringWebapp3Application.class, args);
+	}
+
+}
